8f7f4f958f3d79889e32b97c08918a7eb26622b2
BenjaminAlleauIOD <benjamin.alleau@iod-solutions.fr>
2024-10-03 14:56:09 +0200

    maj socofer

--- Assistant/demonstrateur_assistant.py
+++ Assistant/demonstrateur_assistant.py
@@ -6,8 +6,7 @@ from PIL import Image
 
 
 # Assurez-vous de définir la variable d'environnement OPENAI_API_KEY avant de lancer l'application.
-#token_api = st.secrets["token_api"]
-token_api = "sk-None-4Y3YrFJyPuXzpLoH5afaT3BlbkFJb36ssvdIgcgwFVB3Dj5D"
+token_api = st.secrets["token_api"]
 client = OpenAI(api_key=token_api)
 # Fonction pour attendre la fin de l'exécution
 def wait_on_run(run, thread, client):
@@ -26,7 +25,7 @@ def parse_response(messages):
 
 # Récupération de l'ID de l'assistant stocké
 # Assurez-vous de stocker l'assistant_id dans un endroit sûr et de le charger d'une manière qui ne compromet pas la sécurité.
-assistant_id = "asst_HHZRsqTDcZ2JCjhjSXfs11CG"
+assistant_id = "thread_Y6kqmz3iO5vGhQxf4KQLXbvj"
 
 # Interface Streamlit
 st.set_page_config(layout="wide")
@@ -46,13 +45,13 @@ with col2:
     st.title("Comment puis-je vous aider?")
 # st.image(img)
 # st.title("Bonjour, je suis Ioda! comment puis-je vous aider?")
-st.subheader("Ioda est votre assistant de cuisine")
+st.subheader("Ioda est votre assistant personnalisé pour Socofer")
 
 img_logo = Image.open("Logo_Iod_solutions_Horizontal_Logo_Complet_Blanc_RVB_1186px@72ppi.png")
 st.sidebar.image(img_logo)
 password = st.sidebar.text_input("Entrez le mot de passe", type="password")
 
-if password == "pwd" :#st.secrets["pwd"]:
+if password == st.secrets["pwd"]:
     user_input = st.text_input('Posez votre question ici:')
 
     if st.button('Envoyer'):
